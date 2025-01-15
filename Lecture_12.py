dc7e4c69695583cb2989e4837661265e5c3af3d5
karmaAstra <kumardinesh64469@gmail.com>
2025-01-15 10:30:00 +0530

    Initial commit

---
+++ Lecture_12.py
@@ -0,0 +1,16 @@
+ram_bank_balance = 100000
+ram_loan = 500000
+
+laxshman_bank_balance = 2000000
+laxshman_loan = 1000000
+
+net_income = ram_bank_balance + laxshman_bank_balance
+net_liabilities = ram_loan + laxshman_loan
+final_value = net_income - net_liabilities
+
+print(final_value) # 600000
+print("So the family has:", final_value)
+
+# Tips: Add variables self-explanatory names.
+# Tips: Use comments to explain the code.
+# changes
