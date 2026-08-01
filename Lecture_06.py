8305e9a3d945a343c31a271ac47f97e0720394d9
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:34:06 +0530

    Finished

---
+++ Lecture_06.py
@@ -0,0 +1,10 @@
+n = 10 
+print(n) # Integer
+r = 6.3
+print(r) # Float
+s = "Sudarshan"
+print(s) # String
+
+print("n is of type:", type(n))
+print("r is of type:", type(r))
+print("s is of type:", type(s))
