03ddd1115988e9e117834d7c1edcaa4723606bd4
Dinesh Kumar Mahato <93961152+karmaAstra@users.noreply.github.com>
2026-08-01 20:07:55 +0530

    Update Lecture_1.py

--- Lecture_1.py
+++ Lecture_1.py
@@ -1,1 +1,18 @@
 // Lecture 1
+# a = 10
+# print(a)
+# b = 20
+# print(b)
+# print(a + b)
+# print(a * b)
+
+# a = 10
+# print(a)
+# a = a + 1
+# print(a)
+# a = a + 1 // This is called incrimination in programming
+# print(a)
+
+print("Enter a number:")
+n = int(input())
+print(n)
