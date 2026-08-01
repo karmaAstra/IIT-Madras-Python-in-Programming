69eca80316cc134aa67331746b6127dd84b80b75
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:47:08 +0530

    Finished

--- Lecture_07.py
+++ Lecture_07.py
@@ -5,7 +5,22 @@
 # print("b2 is of type:", type(b2)) # its a boolean
 
 
-a = int(4.5) # type casting
-b = int("10") # type casting
-print("In this 4.5 is Converted to Integer a:",a,"and its type is:",type(a))
-print("In this '10' is Converted to Integer b:",b,"and its type is:",type(b))
+# a = int(4.5) # type casting or type conversion
+# b = int("10") # type casting or type conversion
+# print("In this 4.5 is Converted to Integer a:",a,"and its type is:",type(a))
+# print("In this '10' is Converted to Integer b:",b,"and its type is:",type(b))
+
+# Conversion of Others to boolean
+
+a = bool(0) # False
+b = bool(1) # True
+c = bool("") # False
+d = bool("Sudarshan") # True
+e = bool([]) # False
+f = bool([1,2,3]) # True
+g = bool(None) # False
+h = bool(0.0) # False
+i = bool(-1) # True
+j = bool("0") # True
+k = bool("False") # True
+l = bool("") # False
