bb5d0f1808ad6397572757a65714a1003ae58f58
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:39:34 +0530

    Finished

--- Lecture_06.py
+++ Lecture_06.py
@@ -1,12 +1,22 @@
-n = 10 
-print(n) # Integer
-r = 6.3
-print(r) # Float
-s = "Sudarshan"
-print(s) # String
+# n = 10 
+# print(n) # Integer
+# r = 6.3
+# print(r) # Float
+# s = "Sudarshan"
+# print(s) # String
 
-print("n is of type:", type(n))
-print("r is of type:", type(r))
-print("s is of type:", type(s))
+# print("n is of type:", type(n))
+# print("r is of type:", type(r))
+# print("s is of type:", type(s))
 
-print()
+
+
+# List
+
+l = [10, 20, 30, 40, 50]
+print(l)
+print(l[0]) # Computer Always count from 0
+print(l[1])
+
+print("l is of type:", type(l)) # its a list
+print("l[0] is of type:", type(l[0])) # its a integer
