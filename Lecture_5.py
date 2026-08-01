fef64d6b23800164d1d1b7d6f4bbfa81200e2e3b
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:28:43 +0530

    Updated Lecture 5

--- Lecture_5.py
+++ Lecture_5.py
@@ -3,3 +3,10 @@ location = str(input("Enter your location: "))
 print("Hello", name, "how is the weather in", location, '?')
 age = int(input("Enter your age?: "))
 print("Good to know that you are ", age, "years old")
+
+
+# variable = Literal <-- This particular thing is main
+
+radius = int(input("Enter the radius of the circle: "))
+area = 3.14 * radius * radius
+print("The area of the circle is: ", area)
