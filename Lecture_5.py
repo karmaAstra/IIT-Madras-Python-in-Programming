05b97fa52ca5152bbc9989264f37037c51eb3c7d
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:24:13 +0530

    Update Lecture 5

---
+++ Lecture_5.py
@@ -0,0 +1,5 @@
+name = str(input("Enter your name: "))
+location = str(input("Enter your location: "))
+print("Hello", name, "how is the weather in", location, '?')
+age = int(input("Enter your age?: "))
+print("Good to know that you are ", age, "years old")
