f9e57f13c96238048872e1ff458b7a6674319ae4
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:43:11 +0530

    Finished

---
+++ Lecture_07.py
@@ -0,0 +1,11 @@
+# b1 = True
+# b2 = False
+
+# print("b1 is of type:", type(b1)) # its a boolean
+# print("b2 is of type:", type(b2)) # its a boolean
+
+
+a = int(4.5) # type casting
+b = int("10") # type casting
+print("In this 4.5 is Converted to Integer a:",a,"and its type is:",type(a))
+print("In this '10' is Converted to Integer b:",b,"and its type is:",type(b))
