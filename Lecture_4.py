dc555fbb072023dcdc22826ab35cc43a3a8f33e8
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 02:15:41 +0530

    Update Lecture 4

---
+++ Lecture_4.py
@@ -0,0 +1,7 @@
+print("Hello India","Hello World","Hi Python Students")
+print(10)
+print(10.5)
+
+print("Hello India",20 ,20.5)
+
+print() # Only Round bracket is used not curly bracket or square bracket
