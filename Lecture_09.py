66d2f9769af6a92af3068b9f1db20ddeb59b4d58
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 12:45:20 +0530

    Finished

---
+++ Lecture_09.py
@@ -0,0 +1,40 @@
+# n = 3 + 2
+# print(n)
+
+# a = 1.2
+# b = 2.3
+# n = a + b
+# print(n)
+
+# a = "Sudharshan"
+# b = "India"
+
+# n = a*b
+# print(n) # Error: can't multiply sequence by non-int of type 'str'
+
+
+# a = "Sudharshan"
+# b = "India"
+# n = a + b
+# print(n) # Concatenation of two strings
+
+# a = [1,2,3,4,5]
+# b = [1,4,5]
+# n = a + b
+# print(n) # Concatenation of two lists
+
+# a = 11
+# b = 15
+# n = a/b
+# print(n) # Division of two integers
+
+
+# n = 10 + 13*2
+# print(n) # 36 --> it gives priority to multiplication over addition
+
+# so priority of operations is as follows (This is also called operator precedence):
+# 1. Parenthesis
+# 2. Exponentiation
+# 3. Multiplication and Division
+# 4. Addition and Subtraction
+
