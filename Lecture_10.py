9774d309ebfb30294cf283d3a6e727f5d9540972
karmaAstra <kumardinesh64469@gmail.com>
2026-08-02 13:07:57 +0530

    Finishied

--- Lecture_10.py
+++
@@ -1,9 +0,0 @@
-# Arithmetic (+, -, *, /, //, %, **)
-
-print("Addition", 2 + 3) # 5
-print("Subtraction", 2 - 3) # -1
-print("Multiplication", 2 * 3) # 6
-print("Division", 2 / 3) # 0.6666666666666666 (Float Value)
-print("Floor Division", 2 // 3) # 0 (Integer Value)
-print("Modulus", 2 % 3) # 2
-print("Exponentiation", 2 ** 3) # 8
