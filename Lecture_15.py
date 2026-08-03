571606f34f94e314f74ffee9ab411ce682380278
karmaAstra <kumardinesh64469@gmail.com>
2026-08-03 23:28:20 +0530

    Finished

---
+++ Lecture_15.py
@@ -0,0 +1,7 @@
+a = 10 
+print(type(a)) # <class 'int'> --> Integer data type
+
+a = "India"
+print(type(a)) # <class 'str'> --> String data type
+
+# The change in the data type of the variable 'a' is due to dynamic typing in Python.
