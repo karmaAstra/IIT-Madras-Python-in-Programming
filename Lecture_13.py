c5cf751d62798f920f2468848bdde7911692fb53
karmaAstra <kumardinesh64469@gmail.com>
2026-08-03 21:17:30 +0530

    Finished

---
+++ Lecture_13.py
@@ -0,0 +1,7 @@
+print("Enter your name:")
+n = str(input(""))
+print("Hello,",n, "How are you?")
+# After every ',' in the print statement, a space added.
+print("Which place you are in:")
+p = str(input(""))
+print("Hello,",n, "How are you? You are in",p)
