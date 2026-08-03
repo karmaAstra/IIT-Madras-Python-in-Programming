1906de05681fa54132da4650b6996993e1715480
karmaAstra <kumardinesh64469@gmail.com>
2026-08-03 19:20:31 +0530

    Finished

---
+++ Lecture_10.py
@@ -0,0 +1,22 @@
+#Arthmatic Operations are:
+# + , - , * , / , // , % , **
+
+a = 2
+b = 3 
+print("Addition:", a + b) # 5
+print("Subtraction:", a - b) # -1
+print("Multiplication:", a * b) # 6
+print("Division:", a / b) # 0.6666666666666666
+print("Floor Division:", a // b) # 0
+print("Modulus:", a % b) # 2
+print("Exponentiation:", a ** b) # 8
+
+
+# Relational Operators are:
+# < , > , <= , >= , == , !=
+print("Less than:", a < b) # True
+print("Greater than:", a > b) # False
+print("Less than or equal to:", a <= b) # True
+print("Greater than or equal to:", a >= b) # False
+print("Equal to:", a == b) # False
+print("Not equal to:", a != b) # True
