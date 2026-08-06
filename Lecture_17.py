bd3ed443ba94bbe5065e9d4e1cb1e2e7d2c6c5a6
karmaAstra <kumardinesh64469@gmail.com>
2026-08-06 22:02:58 +0530

    Finished

---
+++ Lecture_17.py
@@ -0,0 +1,18 @@
+s = "this is The String"
+
+print(s.lower()) # lower() --> It converts everything to lowercase
+print(s.upper()) # upper() --> It converts everything to uppercase
+print(s.capitalize()) # Capitalize() --> It converts first letter to uppercase
+print(s.title()) # title() --> It converts every first letter to uppercase
+print(s.swapcase()) # swapcase() --> It swaps the case of the String
+print(s.islower()) # 
+print(s.isupper())
+print(s.istitle())
+print(s.isdigit())
+print(s.isalpha())
+print(s.isalnum())
+print(s.strip("-"))
+print(s.lstrip("-"))
+print(s.rsplit("-"))
+print(s.startswith("t"))
+print(s.endswith("g"))
