dfc183b9abcad2a839d205119ca7b6d81eadac81
karmaAstra <kumardinesh64469@gmail.com>
2026-08-08 22:33:29 +0530

    Finished

--- Lecture_18.py
+++ Lecture_18.py
@@ -7,4 +7,18 @@
 print('it\'s a beautiful day') # \' --> used a single character as '
 print("We are from \"IIT Madras\"") # Allows to use \"
 print("My name is Dinesh. \t I am from Jharkhand") # \t --> Gives a tab space
+print("My name is Dinesh. \n I am from Jharkhand") # \n --> Adds to a new line
 
+
+
+print("It\'s a beautiful day") 
+print('We are from "IIT Madras"') 
+
+x = "This is First String"
+y = 'This is the second String'
+z = ''' This is the third String
+and we can use it here'''
+
+print(x)
+print(y)
+print(z)
