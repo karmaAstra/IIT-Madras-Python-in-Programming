2bf55e005cfd9871b5d0cc7bd68effa84dc0d11d
karmaAstra <kumardinesh64469@gmail.com>
2026-08-08 22:29:15 +0530

    Finished

--- Lecture_18.py
+++ Lecture_18.py
@@ -5,5 +5,6 @@
 # Escape characters are the special methods used to use spacial things
 
 print('it\'s a beautiful day') # \' --> used a single character as '
+print("We are from \"IIT Madras\"") # Allows to use \"
+print("My name is Dinesh. \t I am from Jharkhand") # \t --> Gives a tab space
 
-print("We are from \"IIT Madras\"")
