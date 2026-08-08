89e962123e64cd6ecd8ae34d0364b2360add6994
karmaAstra <kumardinesh64469@gmail.com>
2026-08-08 22:56:43 +0530

    Finished

---
+++ Lecture_20.py
@@ -0,0 +1,11 @@
+alpha ="abcdefghijklmnopqrstuvwxyz"
+
+# i = 25
+# print(alpha[i])
+# print(alpha[i+1])
+# print(alpha[i+2])
+# print(alpha[i+3]) --> it will give a error
+
+i = 27
+
+print(alpha[i%26]) # it wont give error as 27%26 == 1 as it gives the reminded
