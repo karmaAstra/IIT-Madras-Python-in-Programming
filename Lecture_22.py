ee7b91557e00ff73919392594de3635a2b10907d
karmaAstra <kumardinesh64469@gmail.com>
2026-08-09 22:29:05 +0530

    Finished

---
+++ Lecture_22.py
@@ -0,0 +1,16 @@
+# num = int(input("Enter the number:"))
+# if(num%2 == 0):
+#     print("It's a Even")
+# else:
+#     print("It's a Odd")
+
+
+num = int(input("Enter a number:"))
+
+if(num % 5 == 0):
+    if(num % 10 == 0):
+        print('0')
+    else:
+        print('5')
+else:
+    print("Other")
