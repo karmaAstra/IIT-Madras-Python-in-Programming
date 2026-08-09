63dde03174e7b1831ba21d887878fe91ad613b87
karmaAstra <kumardinesh64469@gmail.com>
2026-08-09 22:09:16 +0530

    Finished

--- Lecture_20.py
+++ Lecture_20.py
@@ -10,9 +10,24 @@
 
 # print(alpha[i%26]) # it wont give error as 27%26 == 1 as it gives the reminded.
 
+
+
+
+# This is popularly called as Caesar Cipher in cryptography
 alpha ="abcdefghijklmnopqrstuvwxyz"
 
 s = "dinesh"
 # i will print the sift of 1 latter like ejofti
 
-print(alpha[(alpha.index(s[2])+1)%26])
+t = ""
+i = 0
+k = 1
+
+t = t + alpha[((alpha.index(s[i + 0]))+k)%26]
+t = t + alpha[((alpha.index(s[i + 1]))+k)%26]
+t = t + alpha[((alpha.index(s[i + 2]))+k)%26]
+t = t + alpha[((alpha.index(s[i + 3]))+k)%26]
+t = t + alpha[((alpha.index(s[i + 4]))+k)%26]
+t = t + alpha[((alpha.index(s[i + 5]))+k)%26]
+
+print(t)
