b7c571bab5a7fad1812188205fc4651811324984
karmaAstra <kumardinesh64469@gmail.com>
2026-08-09 22:01:17 +0530

    Finished

--- Lecture_20.py
+++ Lecture_20.py
@@ -1,11 +1,18 @@
+# alpha ="abcdefghijklmnopqrstuvwxyz"
+
+# # i = 25
+# # print(alpha[i])
+# # print(alpha[i+1])
+# # print(alpha[i+2])
+# # print(alpha[i+3]) --> it will give a error
+
+# i = 27
+
+# print(alpha[i%26]) # it wont give error as 27%26 == 1 as it gives the reminded.
+
 alpha ="abcdefghijklmnopqrstuvwxyz"
 
-# i = 25
-# print(alpha[i])
-# print(alpha[i+1])
-# print(alpha[i+2])
-# print(alpha[i+3]) --> it will give a error
+s = "dinesh"
+# i will print the sift of 1 latter like ejofti
 
-i = 27
-
-print(alpha[i%26]) # it wont give error as 27%26 == 1 as it gives the reminded.
+print(alpha[(alpha.index(s[2])+1)%26])
